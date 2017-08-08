864c4514ae4e3d80445b474838b84bcb49d035eb
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-08-08 11:35:46 -0400

    𝔠𝔬𝔪𝔪𝔦𝔱𝔱𝔢𝔡 𝔲𝔰𝔦𝔫𝔤 𝔞𝔯𝔠𝔞𝔫𝔢 𝔪𝔞𝔤𝔦𝔠𝔨𝔰 𝔩𝔬𝔰𝔱 𝔱𝔬 𝔱𝔥𝔦𝔰 𝔪𝔬𝔯𝔱𝔞𝔩 𝔠𝔬𝔦𝔩

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,23 @@
-// Bomb Component Code Goes Here
+import React from 'react'
+
+class Bomb extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render(){
+    let val = `${this.state.secondsLeft} seconds left before I go boom!`
+    if(this.state.secondsLeft === 0){
+      val = 'Boom!'
+    }
+    return (
+      <h1>{val}</h1>
+    )
+  }
+}
+
+export default Bomb
